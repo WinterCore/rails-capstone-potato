2d9c31a0f70c408f5bce3a998ddc9c449d4be17f
WinterCore <hogobbl@gmail.com>
2020-08-11 17:50:06 +0200

    Add vote for article route

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,6 +19,16 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def vote
+    if current_user.votes.exists?(article_id: params[:article_id])
+      flash[:alert] = 'You have already voted for this article!'
+    else
+      current_user.voted_articles << Article.find(params[:article_id])
+      flash[:notice] = 'Vote added successfully.'
+    end
+    redirect_back(fallback_location: root_path)
+  end
+
   private
 
   def article_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
+  resources :articles, only: [:index, :new, :create] do
+    post 'vote', to: 'articles#vote'
+  end
 
   root 'home#index'
 end
