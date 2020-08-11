1f078bdf546b4e3e8cc4fa4766d17836867c76eb
WinterCore <hogobbl@gmail.com>
2020-08-11 13:43:25 +0200

    Add signup page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3, maximum: 255 }
+  validates :username, presence: true, length: { minimum: 3, maximum: 255 }, uniqueness: true
 end
