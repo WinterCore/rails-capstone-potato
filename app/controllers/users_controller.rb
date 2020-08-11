ba521ac1b4eb40e74437e4ae881738b77d12c5e2
WinterCore <hogobbl@gmail.com>
2020-08-11 13:44:30 +0200

    Add user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path, notice: "Account created successfully!"
+      redirect_to root_path, notice: 'Account created successfully!'
     else
       render 'new'
     end
