45aa8a06769710b6c756cc850368cdd83def9fe7
WinterCore <hogobbl@gmail.com>
2020-08-12 18:06:57 +0200

    Add redirect /users to /users/new to prevent the browser from rendering a 404 after refreshing the signup page when it has form errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create]
+  # This is done to prevent the browser from sending a get request to the /users route which doesn't exist
+  # Which causes the browser to render a 404 page
+  get '/users', to: redirect('/users/new')
 
   resources :sessions, only: [:new, :create]
   delete 'sessions', to: 'sessions#destroy'
