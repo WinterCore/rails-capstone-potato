83af32e67b1bdf48a567a433f61225e5793217f3
WinterCore <hogobbl@gmail.com>
2020-08-12 10:07:55 +0200

    Add missing logout link

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create, :destroy]
+
+  resources :sessions, only: [:new, :create]
+  delete 'sessions', to: 'sessions#destroy'
+
   resources :articles, only: [:index, :new, :create] do
     post 'vote', to: 'articles#vote'
   end
